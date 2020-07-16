cadd933d6a55fd62b8a8fd15a736206934a7f54b
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-07-16 13:16:53 -0400

    update debug references

--- test/debug.js
+++ test/debug.js
@@ -1,1 +1,5 @@
+import '/source/main.js';
+
+document.body.style.visibility = 'visible';
+
 document.querySelectorAll('quantum-button').forEach(button => button.addEventListener('click', console.log));
